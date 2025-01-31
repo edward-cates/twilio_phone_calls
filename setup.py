38d686a7a8698d613621059f08089940b0bdae86
Edward Cates <edwardcates7@gmail.com>
2025-01-31 17:16:19 -0600

    version 1.0!

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='twilio_phone_calls',
-    version='0.0',
+    version='1.0',
     packages=['twilio_phone_calls'],
     install_requires=install_requires,
 )
