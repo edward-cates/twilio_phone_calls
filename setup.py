e6f5003a78cfaa20ebf8ac345fe920bf12143a1d
Edward Cates <edwardcates7@gmail.com>
2025-02-14 20:43:22 -0600

    pypi deploy

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 
 setup(
     name='twilio_phone_calls',
-    version='1.3', # Jan 31, 2025.
+    version='2.0', # Feb 15, 2025.
     packages=[
         'twilio_phone_calls',
         'twilio_phone_calls.audio',
         'twilio_phone_calls.twilio_pydantic',
     ],
     install_requires=[
+        'coqui-tts==0.25.3',
         'fastapi==0.115.8',
         'gTTS==2.5.4',
         'librosa==0.10.2.post1',
@@ -22,6 +23,7 @@ setup(
         'scikit-learn==1.6.1',
         'soundfile==0.13.1',
         'SpeechRecognition==3.14.1',
+        'torch==2.6.0',
         'twilio==9.4.4',
         'uvicorn==0.34.0',
         'websockets==14.2',
